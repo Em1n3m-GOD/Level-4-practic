f0e9e5049eab81e9e55c6c1e5249c9cfa2937411
Em1n3m-GOD <evgenijmolotov29@gmail.com>
2026-07-06 19:43:57 +0800

    Исправлены замечания по практике 5

--- app/db/crud.py
+++ app/db/crud.py
@@ -1,6 +1,8 @@
 from app.db.models import Category, Book
 
 
+# ---------- CATEGORY ----------
+
 def create_category(db, title):
     category = Category(title=title)
     db.add(category)
@@ -8,6 +10,37 @@ def create_category(db, title):
     db.refresh(category)
     return category
 
+
+def get_categories(db):
+    return db.query(Category).all()
+
+
+def get_category(db, category_id):
+    return db.query(Category).filter(Category.id == category_id).first()
+
+
+def update_category(db, category_id, title):
+    category = get_category(db, category_id)
+    if not category:
+        return None
+
+    category.title = title
+    db.commit()
+    db.refresh(category)
+    return category
+
+
+def delete_category(db, category_id):
+    category = get_category(db, category_id)
+    if not category:
+        return None
+
+    db.delete(category)
+    db.commit()
+    return category
+
+
+# ---------- BOOK ----------
 
 def create_book(db, title, description, price, url, category):
     book = Book(
@@ -17,15 +50,42 @@ def create_book(db, title, description, price, url, category):
         url=url,
         category=category
     )
+
     db.add(book)
     db.commit()
     db.refresh(book)
     return book
 
 
-def get_categories(db):
-    return db.query(Category).all()
-
-
 def get_books(db):
     return db.query(Book).all()
+
+
+def get_book(db, book_id):
+    return db.query(Book).filter(Book.id == book_id).first()
+
+
+def update_book(db, book_id, title, description, price, url, category):
+    book = get_book(db, book_id)
+    if not book:
+        return None
+
+    book.title = title
+    book.description = description
+    book.price = price
+    book.url = url
+    book.category = category
+
+    db.commit()
+    db.refresh(book)
+    return book
+
+
+def delete_book(db, book_id):
+    book = get_book(db, book_id)
+    if not book:
+        return None
+
+    db.delete(book)
+    db.commit()
+    return book
